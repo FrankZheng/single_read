f6342c578d067173d7393d9a5c823a6e78530b90
Frank Zheng <frank.zheng@vungle.com>
2019-12-25 15:56:41 +0800

    Use flutter web view to show article detail

--- lib/article_detail_view.dart
+++ lib/article_detail_view.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import 'model.dart';
 
@@ -11,15 +14,43 @@ class ArticleDetailView extends StatefulWidget {
 }
 
 class _ArticleDetailViewState extends State<ArticleDetailView> {
+  Widget buildWebView(Article article) {
+    final String url =
+        '${article.html5}?client=iOS&device_id=866963027059338&version=1.3.0';
+    print(url);
+    return WebView(
+      initialUrl: url,
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double top = MediaQuery.of(context).padding.top;
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(
-        child: Center(
-          child: Text('article detail'),
-        ),
+        body: SafeArea(
+      top: false,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: top,
+            color: Colors.black,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: top),
+            child: buildWebView(widget.article),
+          ),
+          Positioned(
+            child: InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.arrow_back_ios, color: Colors.white)),
+            top: top + 5,
+            left: 5,
+          )
+        ],
       ),
-    );
+    ));
   }
 }
--- lib/article_page_view.dart
+++ lib/article_page_view.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:single_read/article_detail_view.dart';
 import 'package:single_read/model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -27,94 +28,108 @@ class _ArticlePageViewState extends State<ArticlePageView> {
     }
   }
 
+  void onArticleTapped(Article article) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => ArticleDetailView(
+                article: article,
+              )),
+    );
+  }
+
   Widget buildBaseArticleContentView(Article article, Widget thumbnailWidget) {
     double width = MediaQuery.of(context).size.width;
-    return Stack(
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            thumbnailWidget,
-            Container(
-              width: width,
-              height: 1,
-              color: Colors.red,
-            ),
-            Container(
-              color: Colors.red,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
-              child: Text(
-                article.category,
-                style: TextStyle(color: Colors.white, fontSize: 12),
+    return InkWell(
+      onTap: () => onArticleTapped(article),
+      child: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              thumbnailWidget,
+              Container(
+                width: width,
+                height: 1,
+                color: Colors.red,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              article.title,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 40),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                article.excerpt == null ? '' : article.excerpt,
+              Container(
+                color: Colors.red,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+                child: Text(
+                  article.category,
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                article.title,
                 textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 40),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  article.excerpt == null ? '' : article.excerpt,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 30),
+                width: 200,
+                height: 1,
+                color: Colors.grey,
+              ),
+              Text(
+                article.author,
                 style: TextStyle(fontSize: 20),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 30),
-              width: 200,
-              height: 1,
-              color: Colors.grey,
-            ),
-            Text(
-              article.author,
-              style: TextStyle(fontSize: 20),
-            ),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.comment,
-                    color: Colors.grey,
-                    size: 20,
-                  ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Text(article.comment),
-                  SizedBox(
-                    width: 30,
-                  ),
-                  Icon(
-                    Icons.favorite_border,
-                    color: Colors.grey,
-                    size: 20,
-                  ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Text(article.good),
-                  Spacer(),
-                  Text(
-                    '${article.view} views',
-                    style: TextStyle(
-                        color: Colors.black, fontStyle: FontStyle.italic),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-        buildTopbar(),
-      ],
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.comment,
+                      color: Colors.grey,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(article.comment),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Icon(
+                      Icons.favorite_border,
+                      color: Colors.grey,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(article.good),
+                    Spacer(),
+                    Text(
+                      '${article.view} views',
+                      style: TextStyle(
+                          color: Colors.black, fontStyle: FontStyle.italic),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          buildTopbar(),
+        ],
+      ),
     );
   }
 
@@ -123,8 +138,8 @@ class _ArticlePageViewState extends State<ArticlePageView> {
       width: width,
       height: height,
       color: Colors.black54,
-      child: CachedNetworkImage(
-        imageUrl: article.thumbnail,
+      child: Image.network(
+        '${article.thumbnail}',
         fit: BoxFit.cover,
       ),
     );
@@ -149,8 +164,7 @@ class _ArticlePageViewState extends State<ArticlePageView> {
           Container(
             width: width,
             height: height,
-            child: CachedNetworkImage(
-                imageUrl: article.thumbnail, fit: BoxFit.fill),
+            child: Image.network('${article.thumbnail}', fit: BoxFit.fill),
           ),
           buildTopbar(),
         ],
